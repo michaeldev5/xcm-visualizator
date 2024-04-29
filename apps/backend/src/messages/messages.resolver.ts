94ad6dcbe11dee5c39a25638058ab165f393d63b
Rikkard29 <absolon.mm@gmail.com>
2024-04-29 15:04:43 +0200

    Add additional xcm messages graphql queries

--- apps/backend/src/messages/messages.resolver.ts
+++ apps/backend/src/messages/messages.resolver.ts
@@ -1,8 +1,11 @@
-import { Args, Int, Query, Resolver } from '@nestjs/graphql';
-import { Message } from './message.entity';
+import { Resolver, Query, Args, Int } from '@nestjs/graphql';
 import { MessageService } from './messages.service';
-import { MessageCount } from './models/message-count.model';
+import { Message } from './message.entity';
+import { AccountXcmCountType } from './models/account-msg-count.model';
+import { AssetCount } from './models/asset-count.model';
+import { MessageCountByDay } from './models/message-count-by-day.model';
 import { MessageCountByStatus } from './models/message-count-by-status.model';
+import { CountOption, MessageCount } from './models/message-count.model';
 
 @Resolver(() => Message)
 export class MessageResolver {
@@ -18,17 +21,6 @@ export class MessageResolver {
     return this.messageService.findOne(message_hash);
   }
 
-  @Query(() => [MessageCount])
-  async totalMessageCounts(
-    @Args('startTime', { type: () => Date }) startTime: Date,
-    @Args('endTime', { type: () => Date }) endTime: Date,
-  ) {
-    return this.messageService.getTotalMessageCounts(
-      startTime.getTime(),
-      endTime.getTime(),
-    );
-  }
-
   @Query(() => [MessageCountByStatus])
   async messageCounts(
     @Args('paraIds', { type: () => [Int], nullable: true }) paraIds: number[],
@@ -40,5 +32,68 @@ export class MessageResolver {
       startTime.getTime(),
       endTime.getTime(),
     );
+  }
+
+  @Query(() => [MessageCountByDay])
+  async messageCountsByDay(
+    @Args('paraIds', { type: () => [Int], nullable: true }) paraIds: number[],
+    @Args('startTime', { type: () => Date }) startTime: Date,
+    @Args('endTime', { type: () => Date }) endTime: Date,
+  ) {
+    const data = await this.messageService.countMessagesByDay(
+      paraIds,
+      startTime.getTime(),
+      endTime.getTime(),
+    );
+
+    return data;
+  }
+
+  @Query(() => [AssetCount])
+  async assetCountsBySymbol(
+    @Args('paraIds', { type: () => [Int], nullable: true }) paraIds: number[],
+    @Args('startTime', { type: () => Date }) startTime: Date,
+    @Args('endTime', { type: () => Date }) endTime: Date,
+  ) {
+    return this.messageService.countAssetsBySymbol(
+      paraIds,
+      startTime.getTime(),
+      endTime.getTime(),
+    );
+  }
+
+  @Query(() => [AccountXcmCountType])
+  async accountCounts(
+    @Args('threshold', { type: () => Int }) threshold: number,
+    @Args('paraIds', { type: () => [Int], nullable: true }) paraIds: number[],
+    @Args('startTime', { type: () => Date }) startTime: Date,
+    @Args('endTime', { type: () => Date }) endTime: Date,
+  ) {
+    return this.messageService.getAccountXcmCounts(
+      paraIds,
+      threshold,
+      startTime.getTime(),
+      endTime.getTime(),
+    );
+  }
+
+  @Query(() => [MessageCount])
+  async totalMessageCounts(
+    @Args('startTime', { type: () => Date }) startTime: Date,
+    @Args('endTime', { type: () => Date }) endTime: Date,
+    @Args('countBy', {
+      type: () => CountOption,
+      defaultValue: CountOption.ORIGIN,
+    })
+    countBy: CountOption,
+  ): Promise<MessageCount[]> {
+    const data = await this.messageService.getTotalMessageCounts(
+      startTime.getTime(),
+      endTime.getTime(),
+      countBy,
+    );
+    console.log(data);
+
+    return data;
   }
 }
--- apps/backend/src/messages/models/message-count.model.ts
+++ apps/backend/src/messages/models/message-count.model.ts
@@ -1,4 +1,15 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { ObjectType, Field, Int, registerEnumType } from '@nestjs/graphql';
+
+export enum CountOption {
+  ORIGIN = 'origin_para_id',
+  DESTINATION = 'dest_para_id',
+  BOTH = 'both',
+}
+
+registerEnumType(CountOption, {
+  name: 'CountOption',
+  description: 'Option to count messages by origin, destination, or both',
+});
 
 @ObjectType()
 export class MessageCount {
